55bac7d5fafb202b76120a2cfd512b0eb39b6b2c
lykov_av <zxandruxazx56@yandex.ru>
2022-08-01 14:57:37 +0500

    test

--- src/Application.java
+++ src/Application.java
@@ -1,6 +1,11 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
+import java.io.FileInputStream;
 import java.security.DigestException;
 
 public class Application extends JFrame {
@@ -99,7 +104,7 @@ public class Application extends JFrame {
         JPanel cardPanel = new JPanel(cardLayout);
 
         JPanel firstLayout = new JPanel();
-        firstLayout.add(new TextField("TThis is first layout"));
+        createContent(firstLayout);
 
         JPanel secondLayout = new JPanel();
         secondLayout.add(new TextField("This is a second layout"));
@@ -304,5 +309,272 @@ public class Application extends JFrame {
     public static void main(String[] args) {
         Application app = new Application();
     }
+
+    public void createContent(JPanel panel) {
+
+        panel.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.BOTH;
+        c.insets = new Insets(10, 10, 10, 10);
+
+        c.weightx = 1;
+        c.weighty = 1;
+        c.gridx = 0;
+        c.gridy = 0;
+
+        JTable table;
+        //panel.add(new TextField("TThis is first layout"));
+        panel.setBackground(Color.GREEN);
+
+        String[] columnNames = {"Name", "Roll Number", "Department"};
+        String[][] data = getData();
+        table = new JTable(data, columnNames);
+        //table.setPreferredSize(new Dimension(Short.MAX_VALUE, Short.MAX_VALUE));
+
+        //table.setMinimumSize(new Dimension(100,100));
+        JScrollPane sp = new JScrollPane(table);
+        //sp.setMinimumSize(new Dimension(100,100));
+
+        panel.add(sp, c);
+
+        JTable table1;
+        table1 = new JTable(data,columnNames);
+        JScrollPane sp1 = new JScrollPane(table1);
+
+        c.weightx = 1;
+        c.weighty = 1;
+        c.gridx = 1;
+        c.gridy = 0;
+        c.gridwidth = GridBagConstraints.RELATIVE;
+
+
+        panel.add(sp1, c);
+
+        JTable table2;
+        table2 = new JTable(data,columnNames);
+        JScrollPane sp2 = new JScrollPane(table2);
+
+        c.gridwidth = 1;
+        c.weightx = 1;
+        c.weighty = 1;
+        c.gridx = 3;
+        c.gridy = 0;
+        c.ipady = 10;
+        c.ipadx = 20;
+
+        panel.add(sp2, c);
+
+        JTable table3;
+        table3 = new JTable(data,columnNames);
+        JScrollPane sp3 = new JScrollPane(table3);
+
+        c.weightx = 1;
+        c.weighty = 1;
+        c.gridx = 0;
+        c.gridy = 1;
+
+        panel.add(sp3, c);
+
+        JTable table4;
+        table4 = new JTable(data,columnNames);
+        JScrollPane sp4 = new JScrollPane(table4);
+
+        c.weightx = 1;
+        c.weighty = 1;
+        c.gridx = 1;
+        c.gridy = 1;
+
+        panel.add(sp4, c);
+
+        JTable table5;
+        table5 = new JTable(data,columnNames);
+        JScrollPane sp5 = new JScrollPane(table5);
+        table5.setBackground(Color.BLACK);
+
+        c.weightx = 1;
+        c.weighty = 1;
+        c.gridx = 3;
+        c.gridy = 1;
+        c.gridwidth = GridBagConstraints.REMAINDER;
+
+        panel.add(sp5, c);
+
+        JTable table6;
+        table6 = new JTable(data,columnNames);
+        JScrollPane sp6 = new JScrollPane(table6);
+        table6.setBackground(Color.YELLOW);
+        table6.setRowSelectionAllowed(false);
+        table6.setCellSelectionEnabled(false);
+        table6.setFocusable(false);
+        table6.setDefaultEditor(Object.class, null);
+
+        table6.setShowGrid(false);
+        table6.getTableHeader().setReorderingAllowed(false);
+        TableColumn column1 = table6.getColumnModel().getColumn(0);
+        column1.setPreferredWidth(600);
+//        column.setMinWidth(500);
+        column1.setMaxWidth(Short.MAX_VALUE);
+        DefaultTableCellRenderer render1 = new DefaultTableCellRenderer();
+        render1.setHorizontalAlignment(JLabel.LEFT);
+        column1.setCellRenderer(render1);
+
+        TableColumn column2 = table6.getColumnModel().getColumn(1);
+        column2.setPreferredWidth(300);
+//        column.setMinWidth(500);
+        column2.setMaxWidth(Short.MAX_VALUE);
+        DefaultTableCellRenderer render2 = new DefaultTableCellRenderer();
+        render2.setHorizontalAlignment(JLabel.CENTER);
+        column2.setCellRenderer(render2);
+
+        TableColumn column3 = table6.getColumnModel().getColumn(2);
+        column3.setPreferredWidth(300);
+//        column.setMinWidth(500);
+        column3.setMaxWidth(Short.MAX_VALUE);
+        DefaultTableCellRenderer render3 = new DefaultTableCellRenderer();
+        render3.setHorizontalAlignment(JLabel.CENTER);
+        column3.setCellRenderer(render3);
+
+        table6.setFont(loadFont());
+        table6.setRowHeight(30);
+        table6.getTableHeader().setFont(loadFont());
+
+
+        UIManager.getDefaults().put("TableHeader.cellBorder" , BorderFactory.createEmptyBorder(0,0,0,0));
+
+
+        c.weightx = 1;
+        c.weighty = 1;
+        c.gridx = 1;
+        c.gridy = 2;
+        c.gridwidth = GridBagConstraints.REMAINDER;
+        c.fill = GridBagConstraints.BOTH;
+        c.insets = new Insets(0, 0, 0, 200);
+
+
+
+        // положение относительно центра
+        // c.anchor = GridBagConstraints.PAGE_END;
+        // нестандартная высота и ширина ячейки
+//        c.ipady = 300;
+//        c.ipadx = 400;
+
+        panel.add(sp6, c);
+
+        JPanel tableWithTitle = new JPanel();
+        JLabel title = new JLabel("Свой титульник", SwingConstants.CENTER);
+        Font f = loadFont();
+        title.setFont(f);
+        JTable table7 = new JTable(data, columnNames);
+        JScrollPane sp7 = new JScrollPane(table7);
+        table7.setBackground(Color.YELLOW);
+        table7.setTableHeader(null);
+//        table7.getTableHeader().setUI(null);
+        tableWithTitle.setLayout(new BorderLayout());
+        tableWithTitle.add(title, BorderLayout.NORTH);
+        tableWithTitle.add(sp7, BorderLayout.CENTER);
+
+        c.insets = new Insets(10, 10, 10, 10);
+
+        c.weightx = 1;
+        c.weighty = 1;
+        c.gridx = 0;
+        c.gridy = 3;
+        c.gridwidth = 1;
+        panel.add(tableWithTitle, c);
+
+    }
+
+    private String[][] getData() {
+        String[][] data = {
+                {"Kundar Kumar Jha" ,"4031", "CSE" },
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"},
+                {"Anand Jha", "6014", "IT"}
+        };
+        return data;
+    }
+
+    private Font loadFont() {
+        Font f = null;
+        try {
+            f = Font.createFont(Font.TRUETYPE_FONT, new FileInputStream(new File("./resources/onest.ttf"))).deriveFont(Font.PLAIN, 24);
+            //f = Font.createFont(Font.TRUETYPE_FONT, new FileInputStream(new File("./resources/FontinSans_Cyrillic_46b/FontinSans_Cyrillic_B_46b.otf"))).deriveFont(Font.PLAIN, 24);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return f;
+    }
+
+    private void setThemeUI() {
+        try {
+            //UIManager.setLookAndFeel( new FlatLightLaf() );
+        } catch( Exception ex ) {
+            System.err.println( "Failed to initialize LaF" );
+        }
+    }
+
 }
 
+class TextParser {
+
+}
